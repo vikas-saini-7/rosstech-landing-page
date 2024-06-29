3edcb797279e6d59356ad78f00470a06145a5209
vikas-saini-7 <vikas289888@gmail.com>
2024-06-29 15:05:18 +0530

    sm

--- src/components/KeyFeatures.jsx
+++ src/components/KeyFeatures.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React from 'react';
+import { motion, useInView } from 'framer-motion';
 
 const FEATURES = [
     {
@@ -18,20 +19,37 @@ const FEATURES = [
     },
 ]
 
+
 const KeyFeatures = () => {
+    const ref = React.useRef(null);
+    const isInView = useInView(ref, { once: false });
+
+    const variants = {
+        hidden: { opacity: 0, x: 50 },
+        visible: { opacity: 1, x: 0 }
+    };
   return (
-    <div className='container mx-auto flex flex-col md:flex-row items-center gap-8'>
+    <div className='container mx-auto flex flex-col md:flex-row items-center gap-8 overflow-hidden'>
         <div className='flex-1'>
             <img src="/key-features.png" alt="" />
         </div>
         <div className='flex-1'>
-            <h1 className='mb-8 text-[36px] font-bold'>Key <span className='text-primary'>features</span> of rosstech</h1>
+            <motion.h1
+                className='mb-8 text-[36px] font-bold'
+                >Key <span className='text-primary'>features</span> of rosstech
+            </motion.h1>
             <div className='flex flex-col gap-6'>
                 {FEATURES.map((item) => (
-                    <div key={item.id} className='flex items-center gap-6'>
+                    <motion.div
+                        ref={ref}
+                        initial="hidden"
+                        animate={isInView ? "visible" : "hidden"}
+                        variants={variants}
+                        transition={{ duration: 1 }}
+                        key={item.id} className='flex items-center gap-6'>
                         <img className='w-8' src={item.image} alt={item.image}/>
                         <p>{item.title}</p>
-                    </div>
+                    </motion.div>
                 ))}
             </div>
         </div>
